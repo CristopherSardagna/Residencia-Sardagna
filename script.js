a52fa07d0eb4f5d475a961042e405fdc7b8942c6
CristopherSardagna <cristopherfsardagna@gmail.com>
2024-08-12 18:30:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,21 +9,3 @@ console.log(window.location.href);
 var url = window.location.href;
 
 console.log(url);
-
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval(function(){
-    nextImage();
-}, 3000)
-
-
-function nextImage(){
-    count++;
-    if(count>11){
-        count = 1;
-    }
-
-    document.getElementById("radio"+count).checked = true;
-
-}
