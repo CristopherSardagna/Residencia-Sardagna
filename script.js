34f795512df4a64977d93c7e72c70df007211da3
CristopherSardagna <cristopherfsardagna@gmail.com>
2024-08-14 18:24:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ console.log(window.location.href);
 var url = window.location.href;
 
 console.log(url);
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
